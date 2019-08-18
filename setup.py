2f88e441175ca464d1586ac61c82e8ddc2742aa5
Deniz Bozyigit <dbozyigit@battrion.com>
2019-08-18 17:33:02 +0200

    uploaded to pypi v0.2.1

--- setup.py
+++ setup.py
@@ -5,21 +5,19 @@
 from setuptools import setup, find_packages
 
 
-with open('README.md') as f:
+with open('README.md', encoding='utf-8') as f:
     readme = f.read()
-
-with open('LICENSE') as f:
-    license = f.read()
 
 setup(
     name='unitdoc',
-    version='0.2.0',
+    version='0.2.1',
     description='A framework that enables classes that describe physical objects with units and easy serialization.',
     long_description=readme,
+    long_description_content_type='text/markdown',   
     author='Deniz Bozyigit',
     author_email='deniz195@gmail.com',
     url='https://github.com/deniz195/unitdoc',
-    license=license,
+    license="MIT",
     packages=find_packages(exclude=('tests', 'docs', 'examples')),
     install_requires = ['attrs', 'cattrs', 'attr-descriptions>=0.1.3', 'ruamel.yaml', 'pint_mtools'],
     classifiers=[
